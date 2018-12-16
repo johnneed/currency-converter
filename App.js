9c9bf65011abfac495b950b9a493b405a3b49158
John Need <johnneed@johnneed.com>
2018-12-16 16:07:28 -0500

    first draft

--- App.js
+++ App.js
@@ -7,34 +7,55 @@ import React from 'react';
 import {WorldMap} from './components/world-map';
 import {
     TouchableHighlight,
+    TextInput,
     StyleSheet,
+    Modal,
     Text,
-    View,
+    View
 } from 'react-native';
 import {countries} from './data/countries';
 
 const api = 'http://data.fixer.io/api/latest?access_key=093403968e43916d2e1f9b0cdb61fe4b';
 
 const styles = StyleSheet.create({
-    container: {flex: 1, paddingTop: 20},
-    footer: {
-        height: 100
-    }
+    button: {height: 40, width: 50, backgroundColor: '#EEE', marginLeft: 10, borderColor: '#AAA', borderWidth: 1},
+    buttonText: {textAlign: 'center', lineHeight: 40, height: 40},
+    container: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center'
+
+    },
+    control: {flex: 1, flexDirection: 'row', justifyContent: 'center'},
+    countryName: {textAlign: 'center', height: 20},
+    label: {flex: 1, height: 60, alignItems: 'stretch', justifyContent: 'center'},
+    input: {height: 40, width: 60, borderColor: 'gray', borderWidth: 1, textAlign: 'center'}
 });
+
+const formatNumber = (text) => {
+    const temp = parseFloat(text);
+    if (!isNaN(temp)) {
+        const splits = temp.toString().split('.');
+        return `${splits[0]}${text.indexOf('.') > -1 ? '.' : ''}${(splits[1] || '').slice(0, 2)}`;
+    }
+    return '';
+};
 
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.setCountry = this.setCountry.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.convert = this.convert.bind(this);
     }
 
-    state = {rates: null, fromTo: 'from', to: null, from: null};
+    state = {rates: null, fromTo: 'from', to: 'CA', from: 'US', modalVisible: false};
 
     componentDidMount() {
         fetch(api)
             .then(response => response.json())
-            .then(rates => this.setState({rates}));
+            .then(rates => this.setState({rates: rates.rates}));
     }
 
     setCountry(toFrom) {
@@ -44,18 +65,73 @@ export default class App extends React.Component {
         };
     }
 
+
+    toggleModal(settings) {
+        this.setState({...settings, modalVisible: !this.state.modalVisible});
+    }
+
+
+    convert() {
+        const {amount, rates, from, to} = this.state;
+        const fromCurrency = ((countries[from] || {}).currency || {}).code;
+        const toCurrency = ((countries[to] || {}).currency || {}).code;
+        const _amount = parseFloat(amount);
+        const fromRate = rates && rates[fromCurrency];
+        const toRate = rates && rates[toCurrency];
+
+        return Boolean(!isNaN(_amount) && fromRate && toRate) ? Math.round(amount / fromRate * toRate * 100) / 100 : '';
+    }
+
     render() {
-        const fromCurrency = ((countries[this.state.from] || {}).currency || {}).name;
-        const toCurrency = ((countries[this.state.to] || {}).currency || {}).name;
+        const fromCurrency = ((countries[this.state.from] || {}).currency || {}).code;
+        const toCurrency = ((countries[this.state.to] || {}).currency || {}).code;
+        const fromCountry = (countries[this.state.from] || {}).name;
+        const toCountry = (countries[this.state.to] || {}).name;
 
         return (
             <View style={styles.container}>
-                <View style={styles.footer}>
-                    <Text>{fromCurrency || ''}</Text>
-                    <Text>{toCurrency || ''}</Text>
+                <View style={{height: 210, backgroundColor: '#EEE'}}>
+                    <Text style={{fontSize: 30, lineHeight: 40, textAlign: 'center'}}>{'Currency Converter'}</Text>
+                    <View style={styles.label}>
+                        <Text style={styles.countryName}>{fromCountry || ''}</Text>
+                        <View style={styles.control}>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(text) => this.setState({amount: formatNumber(text)})}
+                                value={this.state.amount}
+                            />
+                            <TouchableHighlight
+                                onPress={() => this.toggleModal({fromTo: 'from'})}
+                                style={styles.button}>
+                                <Text style={styles.buttonText}>{fromCurrency || ''}</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                    <View>
+                        <Text
+                            style={{textAlign: 'center', height: 50, lineHeight: 50}}>{' is the equivalent of '}</Text>
+                    </View>
+                    <View style={styles.label}>
+                        <View style={styles.control}>
+                            <Text style={[styles.input, {borderColor: '#DDD', lineHeight: 40}]}>
+                                {this.convert()}
+                            </Text>
+                            <TouchableHighlight
+                                onPress={() => this.toggleModal({fromTo: 'to'})}
+                                style={styles.button}>
+                                <Text style={styles.buttonText}>{toCurrency || ''}</Text>
+                            </TouchableHighlight>
+                        </View>
+                        <Text style={styles.countryName}>{toCountry || ''}</Text>
+                    </View>
                 </View>
-                <WorldMap onSelect={this.setCountry(this.state.fromTo)}/>
-
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={this.toggleModal}>
+                    <WorldMap close={this.toggleModal} onSelect={this.setCountry(this.state.fromTo)}/>
+                </Modal>
             </View>
         );
     }
--- components/world-map.js
+++ components/world-map.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {MapView} from 'expo';
 import PropTypes from 'prop-types';
+import {View, StyleSheet, TouchableOpacity,Text} from 'react-native';
 
 const apiURL = 'http://api.geonames.org/findNearbyJSON?lat={lat}&lng={long}&username=johnneed';
 
@@ -9,9 +10,15 @@ const findCountry = (location) => {
     return fetch(url).then(response => response.json());
 };
 
+const styles = StyleSheet.create(
+    {
+        container: {flex: 1}
+    });
+
 export class WorldMap extends Component {
 
     static propTypes = {
+        close: PropTypes.func,
         onSelect: PropTypes.func
     };
 
@@ -27,17 +34,20 @@ export class WorldMap extends Component {
             const countryCode = _data.geonames[0].countryCode;
             this.props.onSelect(countryCode);
         });
+        this.props.close();
     }
 
     render() {
         return (
-            <MapView
-                style={{flex: 1}}
-                showsUserLocation={true}
-                showsMyLocationButton={true}
-                showsCompass={true}
-                onPress={this.onMapTap}
-            />
+            <View style={styles.container}>
+                <MapView
+                    style={{flex: 1}}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    showsCompass={true}
+                    onPress={this.onMapTap}
+                />
+            </View>
         );
     }
 
